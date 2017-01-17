8764931ff274e163cb3caa9af341355f9495f331
Pedro Henrique Cruz Caminha <cruz@niteroi.gta.ufrj.br>
2017-01-17 11:58:33 -0200

    Starting models

--- publishing/sensing_bus/publisher/models.py
+++ publishing/sensing_bus/publisher/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from djangotoolbox.fields import EmbeddedModelField
 
-# Create your models here.
+class Location(models.Model):
+    """Represents a point in latitude and longitude"""
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lng = models.DecimalField(max_digits=9, decimal_places=6)
+
+class Bus(models.Model):
+    """Represents a bus carrying a sensor node"""
+    name = models.CharField(max_length=10)
+
+class Stop(model.Model):
+    """Represents a bus stop having a flushing node"""
+    location = EmbeddedModelField('Location')
+
+class Context(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    bus = EmbeddedModelField('Bus')
+    time = models.DateTimeField()
+
+
+class Measurement(models.Model):
+    """Represents the measurements made by a bus on a given time and place"""
+    context = EmbeddedModelField('Context')
+
+    temperature = models.DecimalField(max_digits=4, decimal_places=1)
+    humidity = models.DecimalField(max_digits=4, decimal_places=1)
+    light = models.DecimalField(max_digits=4, decimal_places=1)
+    rain = models.DecimalField(max_digits=4, decimal_places=1)
--- publishing/sensing_bus/sensing_bus/urls.py
+++ publishing/sensing_bus/sensing_bus/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sensing_bus.views.home', name='home'),
+    url(r'^$', include('publisher.urls')),
     # url(r'^sensing_bus/', include('sensing_bus.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
