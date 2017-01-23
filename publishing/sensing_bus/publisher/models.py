f6a176302906424a9181c0309f96c7b891328d63
Pedro Henrique Cruz Caminha <cruz@niteroi.gta.ufrj.br>
2017-01-23 11:15:12 -0200

    Eliminate populate script

--- publishing/sensing_bus/publisher/models.py
+++ publishing/sensing_bus/publisher/models.py
@@ -5,10 +5,17 @@ class Bus(models.Model):
     """Represents a bus carrying a sensor node"""
     name = models.CharField(max_length=10, unique=True)
 
+    def __unicode__(self):
+        return self.name
+
 class Stop(models.Model):
     """Represents a bus stop having a flushing node"""
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lng = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __unicode__(self):
+        return "{},{}".format(self.lat, self.lng)
+
 
 class Measurement(models.Model):
     """Represents the measurements made by a bus on a given time and place"""
@@ -19,9 +26,14 @@ class Measurement(models.Model):
     stop = models.ForeignKey(Stop, on_delete=models.CASCADE,
              verbose_name="stop that received measurement")
     time = models.DateTimeField()
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lng = models.DecimalField(max_digits=9, decimal_places=6)
 
     #Measurements
     temperature = models.DecimalField(max_digits=4, decimal_places=1, null=True)
     humidity = models.DecimalField(max_digits=4, decimal_places=1, null=True)
     light = models.DecimalField(max_digits=4, decimal_places=1, null=True)
     rain = models.DecimalField(max_digits=4, decimal_places=1, null=True)
+
+    def __unicode__(self):
+        return "{},{},{}".format(self.bus,self.stop,self.time)
