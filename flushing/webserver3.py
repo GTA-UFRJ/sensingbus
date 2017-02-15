ee71c12e74e2dd5b28f9937cb9e678bd62d52bf6
Pedro Henrique Cruz Caminha <cruz@niteroi.gta.ufrj.br>
2017-02-15 16:46:38 -0200

    Webserver authenticates to apache with certificates

--- flushing/webserver3.py
+++ flushing/webserver3.py
@@ -12,48 +12,29 @@ from urlparse import parse_qs
 from threading import Timer
 import json
 import requests
+
 signal(SIGPIPE, SIG_DFL)
-TASK_MANAGER_IP = '146.164.69.201'
-json_vect = []
-TIME_CONFIG = 60
-cont = 0    
-def set_interval(function, interval, *params, **kwparams):
-	def set_timer(wrapper):
-		wrapper.timer = Timer(interval, wrapper)
-		wrapper.timer.start()
-	def wrapper():
-		function(*params, **kwparams)
-		set_timer(wrapper)
-	set_timer(wrapper)
-	return wrapper
-def clear_interval(wrapper):
-	wrapper.timer.cancel()
+
+MEASUREMENTS_URL = 'https://sensingbus.gta.ufrj.br/measurements_batch_sec'
+SERVER_CERTIFICATE='/home/pi/ssl/ca-chain.cert.pem'
+
+# Variables for server-side validation:
+#MEASUREMENTS_URL = 'https://146.164.69.186/measurements_batch_sec/'
+#PRIMARY_KEY='/home/pi/ssl/raspberry1.key.pem'
+#LOCAL_CERTIFICATE='/home/pi/ssl/raspberry1.cert.pem'
+
 
 def cloud_client(payload):
     """ Sends mensage to Cloud"""
-    import requests
 
     headers = {'Content-Type' : 'application/json', 
                 'Content-Length': str(len(payload))}
-    r = requests.post('http://sensingbus.gta.ufrj.br/measurements_batch_sec/', json.loads(payload), headers=headers)
-
-def send_to_arduino(mode, ip_host, port, msg):
-    """ Sends a configutarion parameters to Arduino by Socket """
-    import socket
-
-    if ( mode == 'SocketTCP'):
-        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        destination = (ip_host, port)
-        tcp.connect(destination)
-        tcp.send(msg)
-        tcp.close()
-    elif ( mode == 'SocketUDP'):
-        udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        destination = (ip_host, port)
-        udp.sendto(msg, destination)
-        udp.close()
-    else:
-        cloud_client(ip_host, port, msg)
+    r = requests.post(MEASUREMENTS_URL,
+                    json.loads(payload),
+                    headers=headers,
+                    verify=SERVER_CERTIFICATE,
+                    cert=(LOCAL_CERTIFICATE, PRIMARY_KEY))
+    print r
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self): 
@@ -66,58 +47,33 @@ class S(BaseHTTPRequestHandler):
         self._set_headers()
         f = open("index.html", "r")
         self.wfile.write(f.read())
-    
+
     def do_HEAD(self):
         self._set_headers()
-    
+
     def do_POST(self): 
         """Receives POST method"""
-	cont = 0
         postvars = parse_qs(
                 self.rfile.read(int(self.headers['Content-Length'])), 
                 keep_blank_values=1)
-        #print postvars
-	#print json.dumps(postvars)
+        print "Originalmente: "
+        print postvars
+        print "Des-json: "
+        print json.dumps(postvars)
         self.send_response(200)
         self.end_headers()
         self.wfile.write("Ok")
-	json_vect.append(postvars)
-	json_input = '{"stop_id": 1, "batches": [%s ] }'%(json.dumps(json_vect))
-	print json.loads(json_input)
-        #set_interval(cloud_client(json_input), 60)
-	while (cont < TIME_CONFIG):
-			
-		cloud_client(json_input)	
-		time.sleep(1)
-		cont = cont + 1
-		
+        json_vect.append(postvars)
+        json_input = '{"stop_id": 1, "batches": [%s ] }'%(json.dumps(json_vect))
+        print json.loads(json_input)
         return
 
-
-        '''if (data['module_id'] == '0000'):
-            print 'nuvem'
-            module_id = data['id_arduino']
-        else:
-            print 'arduino'
-            print data['data'][0]['temperature']
-            ClientJson(self.ip, self.port, data)
-            temperature = data['data'][0]['temperature']
-            module_id = data['module_id']
-            if found:
-                print 'encontrado'
-            else:
-                print 'nao encontrado'
-            pass'''
-    
 def run(server_class=HTTPServer, handler_class=S, port=50000):
-    """ generates a server to receive POST method"""
+    """Generates a server to receive POST method"""
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print 'Iniciando Server Http'
-    httpd.serve_forever()    
+    httpd.serve_forever()
 
 if __name__ == "__main__":
     run()
-    
-    #cloud_client(TASK_MANAGER_IP, 8000, 'msg')
-
