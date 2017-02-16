9cc741916e60bdbf09375f9cb411deaea1947249
pachecobeto95 <robertopvc@gmail.com>
2017-02-16 16:46:57 +0000

    Implements aggregation

--- flushing/webserver3.py
+++ flushing/webserver3.py
@@ -1,3 +1,5 @@
+
+
 #Author: Roberto Goncalves Pacheco
 #Universidade do Estado do Rio de Janeiro
 #Departamento de Eletronica e Telecomunicacoes
@@ -13,15 +15,31 @@ import json
 import requests
 import time
 import datetime
+import threading, Queue
 
 signal(SIGPIPE, SIG_DFL)
 SERVER_CERTS = '/home/pi/ssl/ca-chain.cert.pem'
 STOP_ID = 1
 MEASUREMENTS_URL = 'https://sensingbus.gta.ufrj.br/measurements_batch_sec/'
 # Variables for server-side validation:
-#MEASUREMENTS_URL = 'https://146.164.69.186/measurements_batch_sec/'
 PRIMARY_KEY='/home/pi/ssl/raspberry.key.pem'
 LOCAL_CERTIFICATE='/home/pi/ssl/raspberry.cert.pem'
+
+q = Queue.Queue()
+
+def send_thread(thread_name,q):
+	"""Sends periodically stored data"""
+	while True:
+		output = {}
+		output['stop_id'] = STOP_ID
+		output['batches'] = []
+
+		while not q.empty():
+			b = q.get()
+			if ( b is not None):
+				output['batches'].append(b)
+		cloud_client(output)	
+		time.sleep(120)
 
 def cloud_client(payload):
     """ Sends mensage to Cloud"""
@@ -40,16 +58,14 @@ class S(BaseHTTPRequestHandler):
 
     def do_POST(self): 
         """Receives data from Arduino and sends to Cloud"""
-	output = {}
 	input_batches = {}
-	output['stop_id'] = STOP_ID
-	output['batches'] = []
         postvars = parse_qs(self.rfile.read(int(self.headers['Content-Length'])), keep_blank_values=1)
 	input_batches['node_id'] = postvars['node_id'][0]
 	for line in postvars['load']:
 		tmp = line.split('\n')
 
 	#delete data defective date
+	print 'received post'
 	delete_list = []
 	for i in range(len(tmp)):
 		if (tmp[i][0:-1] == 0):
@@ -61,17 +77,18 @@ class S(BaseHTTPRequestHandler):
 	input_batches['header'] = str(postvars['header'][0])
 	input_batches['received'] = str(datetime.datetime.now())
 	input_batches['load'] = tmp[0:-1] #the last line is always empty 
-	output['batches'].append(input_batches)
-	print output
-	cloud_client(output)	
+	q.put(input_batches)	
         return
 
 def run(server_class=HTTPServer, handler_class=S, port=50000):
     """Generates a server to receive POST method"""
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print 'Iniciando Server Http'
+    print 'Starting Server Http'
+    t = threading.Thread( target = send_thread, args=('alt',q))
+    t.daemon = True
+    t.start()
     httpd.serve_forever()
-
+    t.join()
 if __name__ == "__main__":
     run()
