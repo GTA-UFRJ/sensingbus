4d78d59665563863c0d01ac0fde75d6e87fe093a
Pedro Henrique Cruz Caminha <cruz@niteroi.gta.ufrj.br>
2017-03-21 11:51:44 -0300

    solving errors

--- publishing/sensing_bus/publisher/urls.py
+++ publishing/sensing_bus/publisher/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
-    url(r'^visualize/', views.zz, name='visualize'),
-    url(r'^docs/', include('rest_framework_docs.urls')),
+    url(r'^visualize/', views.visualize, name='visualize'),
+    url(r'^docs/',  views.docs, name='docs'),
     url(r'^measurements/$',views.measurement_list),
     url(r'^measurements_batch/$',views.measurement_batch_list),
     url(r'^measurements_batch_sec/$',views.measurement_batch_list),
