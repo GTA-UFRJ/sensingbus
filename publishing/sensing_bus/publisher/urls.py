a38dded68a4da0f86e057a9b3e86e96a9d0746b9
Pedro Henrique Cruz Caminha <cruz@niteroi.gta.ufrj.br>
2017-03-21 11:49:54 -0300

    Returning to function-based views

--- publishing/sensing_bus/publisher/urls.py
+++ publishing/sensing_bus/publisher/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
-    url(r'^visualize/', views.VisualizeData.as_view()),
+    url(r'^visualize/', views.zz, name='visualize'),
     url(r'^docs/', include('rest_framework_docs.urls')),
     url(r'^measurements/$',views.measurement_list),
     url(r'^measurements_batch/$',views.measurement_batch_list),
--- publishing/sensing_bus/publisher/views.py
+++ publishing/sensing_bus/publisher/views.py
@@ -11,9 +11,6 @@ from rest_framework.parsers import JSONParser
 from publisher.serializers import MeasurementSerializer
 from publisher.serializers import MeasurementBatchList
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
 import datetime
 
 from .forms import VisualizeForm
@@ -30,66 +27,64 @@ def docs(request):
     context = {}
     return render(request, 'publisher/docs.html', context)
 
+def visualize(request):
+    """Renders the visualization webpage on GET and returns map data on POST"""
+    form = VisualizeForm()
+    context = {'form': form}
 
-class VisualizeData(APIView):
-    def get(self, request, format=None):
-        """Renders the visualization webpage on GET and returns map data on POST"""
-        form = VisualizeForm()
-        context = {'form': form}
+    if request.method == 'GET':
+        return render(request, 'publisher/visualize.html', context)
+    if request.method == 'POST':
+        bus_name = request.POST.get("id_bus_name", "")
+        min_lat = request.POST.get("id_min_lat", "")
+        max_lat = request.POST.get("id_max_lat", "")
+        min_lng = request.POST.get("id_min_lng", "")
+        max_lng = request.POST.get("id_max_lng", "")
+        start_time = request.POST.get("id_start_time", "")
+        end_time = request.POST.get("id_end_time", "")
+        sensor_name = request.POST.get("id_sensor_name_0", "")
 
-        if request.method == 'GET':
-            return render(request, 'publisher/visualize.html', context)
-        if request.method == 'POST':
-            bus_name = request.POST.get("id_bus_name", "")
-            min_lat = request.POST.get("id_min_lat", "")
-            max_lat = request.POST.get("id_max_lat", "")
-            min_lng = request.POST.get("id_min_lng", "")
-            max_lng = request.POST.get("id_max_lng", "")
-            start_time = request.POST.get("id_start_time", "")
-            end_time = request.POST.get("id_end_time", "")
-            sensor_name = request.POST.get("id_sensor_name_0", "")
+        q = Measurement.objects.all()
 
-            q = Measurement.objects.all()
+        if bus_name:
+            bus_key = Bus.objects.filter(name__iexact=bus_name).first().pk
+            q = q.filter(bus=bus_key)
+        if min_lat:
+            q = q.filter(lat__gte=min_lat)
+        if max_lat:
+            q = q.filter(lat__lte=max_lat)
+        if min_lng:
+            q = q.filter(lat__gte=min_lng)
+        if max_lng:
+            q = q.filter(lat__lte=max_lng)
+        if start_time:
+            q = q.filter(time__gte=start_time)
+        if end_time:
+            q = q.filter(time__lte=end_time)
 
-            if bus_name:
-                bus_key = Bus.objects.filter(name__iexact=bus_name).first().pk
-                q = q.filter(bus=bus_key)
-            if min_lat:
-                q = q.filter(lat__gte=min_lat)
-            if max_lat:
-                q = q.filter(lat__lte=max_lat)
-            if min_lng:
-                q = q.filter(lat__gte=min_lng)
-            if max_lng:
-                q = q.filter(lat__lte=max_lng)
-            if start_time:
-                q = q.filter(time__gte=start_time)
-            if end_time:
-                q = q.filter(time__lte=end_time)
+        data = {'data' : []}
+        for o in q:
+            d = {}
+            d['lat'] = float(o.lat)
+            d['lng'] = float(o.lng)
+            if sensor_name == 'TEMPERATURE':
+                d['value'] = float(o.temperature)
+            if sensor_name == 'HUMIDITY':
+                d['value'] = float(o.humidity)
+            if sensor_name == 'LIGHT':
+                d['value'] = float(o.light)
+            if sensor_name == 'RAIN':
+                d['value'] = float(o.rain)
+            data['data'].append(d)
 
-            data = {'data' : []}
-            for o in q:
-                d = {}
-                d['lat'] = float(o.lat)
-                d['lng'] = float(o.lng)
-                if sensor_name == 'TEMPERATURE':
-                    d['value'] = float(o.temperature)
-                if sensor_name == 'HUMIDITY':
-                    d['value'] = float(o.humidity)
-                if sensor_name == 'LIGHT':
-                    d['value'] = float(o.light)
-                if sensor_name == 'RAIN':
-                    d['value'] = float(o.rain)
-                data['data'].append(d)
+        data['max'] = 30
+        now = datetime.datetime.now()
+        html = "Data = %s" % data
+        return JsonResponse(data)
 
-            data['max'] = 30
-            now = datetime.datetime.now()
-            html = "Data = %s" % data
-            return JsonResponse(data)
-
-        else:
-            html = "<html><body>This is not nice, lek</body></html>"
-            return HttpResponse(html)
+    else:
+        html = "<html><body>This is not nice, lek</body></html>"
+        return HttpResponse(html)
 
 class JSONResponse(HttpResponse):
     """
