41378729ae31c54ed3079b8e0933f10fda9113f4
Pedro Henrique Cruz Caminha <cruz@niteroi.gta.ufrj.br>
2017-03-31 18:17:37 -0300

    eliminationg unsecure data receiver

--- publishing/sensing_bus/publisher/urls.py
+++ publishing/sensing_bus/publisher/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^visualize/', views.visualize, name='visualize'),
     url(r'^docs/',  views.docs, name='docs'),
     url(r'^measurements/$',views.measurement_list),
-    url(r'^measurements_batch/$',views.measurement_batch_list),
+    #url(r'^measurements_batch/$',views.measurement_batch_list),
     url(r'^measurements_batch_sec/$',views.measurement_batch_list),
     url(r'^measurements/(?P<pk>[0-9]+)/$', views.measurement_detail),
 ]
