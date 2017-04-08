e2f2b9c89395405d49b3525dce352b033e655dee
Pedro Henrique Cruz Caminha <cruz@niteroi.gta.ufrj.br>
2017-04-08 15:00:06 -0300

    Debugging the info windows

--- publishing/sensing_bus/static/js/visualize.js
+++ publishing/sensing_bus/static/js/visualize.js
@@ -22,6 +22,32 @@ heatmap = new HeatmapOverlay(map,
   }
 );
 
+markers = []
+
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(map);
+    }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+    setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+
 
 var frm = $('#visualization-search');
 frm.submit(function () {
@@ -35,55 +61,52 @@ frm.submit(function () {
             console.log("Data:");
             console.log(data);
             heatmap.setData(data);
-            //Here we draw circles.
-            for (var i = 0; i < data.data.length; i++){
+            var infowindow = new google.maps.InfoWindow();
+
+            function placeMarker(info){
 
                 var contentString = '<div id="content">'+
                     '<div id="siteNotice">'+
                     '</div>'+
                     '<div id="bodyContent">'+
-                    '<p><b>Temperature: </b><p>' + data.data[i].temperature + '</p>' +
-                    '<p><b>Humidity: </b><p>' + data.data[i].humidity + '</p>' +
-                    '<p><b>Rain: </b><p>' + data.data[i].rain + '</p>' +
-                    '<p><b>Light intensity: </b><p>' + data.data[i].light + '</p>' +
+                    '<p><b>Temperature: </b><p>' + info.temperature + '</p>' +
+                    '<p><b>Humidity: </b><p>' + info.humidity + '</p>' +
+                    '<p><b>Rain: </b><p>' + info.rain + '</p>' +
+                    '<p><b>Light intensity: </b><p>' + info.light + '</p>' +
                     '</div>'+
                     '</div>';
-                var infowindow = new google.maps.InfoWindow({
-                  content: contentString
-                });
+
 
                 var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(data.data[i].lat, data.data[i].lng),
+                    position: new google.maps.LatLng(info.lat, info.lng),
                     icon: {
                         path: google.maps.SymbolPath.CIRCLE,
                         strokeColor: '#FF0000',
                         strokeOpacity: 0,
                         strokeWeight: 1,
                         fillColor: '#FF0000',
-                        fillOpacity: 1,
+                        fillOpacity: 0,
                         scale: 3
                     },
                     map: map,
                 });
 
-                marker.addListener('click', function() {
+                google.maps.event.addListener(marker, 'click', function(){
+                    infowindow.close(); // Close previously opened infowindow
+                    infowindow.setContent(contentString);
                     infowindow.open(map, marker);
                 });
-                /*var measurementCircle = new google.maps.Circle({
-                    strokeColor: '#FF0000',
-                    strokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    fillColor: '#FF0000',
-                    fillOpacity: 0.35,
-                    map: map,
-                    center: new google.maps.LatLng(data.data[i].lat, data.data[i].lng),
-                    radius: 5
-                });
 
-                measurementCircle.addListener('click', function() {
-                    infowindow.setPosition(measurementCircle.getCenter());
-                    infowindow.open(map, measurementCircle);
-                });*/
+                markers.push(marker);
+            }
+            //Here we draw markers
+            for (var i = 0; i < data.data.length; i++){
+
+                placeMarker(data.data[i])
+
+
+
+                
             }
         },
         error: function(data) {
