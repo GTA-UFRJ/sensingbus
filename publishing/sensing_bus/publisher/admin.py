6b47cf9662f341f70aad40b51c898a118c971382
Pedro Cruz <cruz@gta.ufrj.br>
2018-04-27 11:11:58 -0300

    Changing dir names

--- publishing/sensing_bus/publisher/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Measurement, Bus, Stop, SensingNode
-
-admin.site.register(Measurement)
-admin.site.register(Bus)
-admin.site.register(Stop)
-admin.site.register(SensingNode)
